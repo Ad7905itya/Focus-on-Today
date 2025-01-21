baa24534a188fafd69537130171314cb1acab5bc
Aditya singh <6306043783s@gmail.com>
2025-01-21 20:27:59 +0530

    Add GitHub repository Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
 import FocusContainer from './components/FocusContainer'
 
 function App() {
 
   return (
     <main className='flex flex-col items-center bg-[#48A30012] px-8 pt-10 min-h-screen'>
-      <h1 className='font-bold text-[#48A300] text-4xl md:text-6xl'>Focus on <span className='text-[#61481C]'>Today</span></h1>
+      <h1 className='font-bold text-[#48A300] text-4xl md:text-6xl'>
+        Focus on
+        <span className='text-[#61481C]'>Today</span>
+      </h1>
       <FocusContainer />
+      <h1 className='py-3'>GitHub Link :&nbsp;
+        <a
+          target='_blank'
+          className='text-sky-600'
+          href='https://github.com/Ad7905itya/Focus-on-Today'>https://github.com/Ad7905itya/Focus-on-Today</a>
+      </h1>
     </main>
   )
 }
